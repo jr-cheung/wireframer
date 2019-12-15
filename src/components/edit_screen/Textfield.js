1ec4f79ead339a6e8c64e117c8695777c4a2fcc6
Your Name <you@example.com>
2019-12-15 17:54:28 -0500

    changing properties of any object fully functional

--- src/components/edit_screen/Textfield.js
+++ src/components/edit_screen/Textfield.js
@@ -2,6 +2,27 @@ import React from 'react';
 import Draggable from 'react-draggable'
 
 class Textfield extends React.Component {
+    handleSelect = () =>{
+        const thisTextfield = this.props.controlObj;
+        this.props.processComponentSelectedTextfield(thisTextfield);
+    }
+
+    divStyle = () =>{
+        const thisTextfield = this.props.controlObj;
+        return {cursor: 'grab',
+            zIndex: '2',
+            borderStyle: 'solid',
+            
+            backgroundColor: thisTextfield.background_color,
+            borderColor: thisTextfield.border_color,
+            borderWidth: thisTextfield.border_thickness,
+            borderRadius: thisTextfield.border_radius,
+            fontSize: thisTextfield.font_size,
+            color: thisTextfield.text_color,
+            width: '250px'
+        };
+    }
+
     render() {
         return (
             <Draggable
@@ -12,7 +33,9 @@ class Textfield extends React.Component {
             onDrag={this.handleDrag}
             onStop={this.handleStop}>
             <div>
-            <div className="control_textfield">Input</div>
+            <div
+                style={this.divStyle()}
+                onClick={this.handleSelect}>{this.props.controlObj.text}</div>
             </div>
         </Draggable>
         );
