d2241e096a514073d270abb1482de01e210b9318
Your Name <you@example.com>
2019-12-15 22:58:37 -0500

    pushed renamed files

---
+++ src/components/home_screen/DiagramCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { getFirestore } from 'redux-firestore';
+
+class DiagramCard extends React.Component {
+
+    updateTimestamp() {
+        const {diagram} = this.props;
+        const id = diagram.id;
+        const fireStore = getFirestore();
+        fireStore.collection('diagrams').doc(id).update({timestamp: Date.now()});
+    }
+
+    render() {
+        const { diagram } = this.props;
+        console.log("DiagramCard, diagram.id: " + diagram.id);
+        return (
+            <div className="card z-depth-0 todo-list-link" onClick={this.updateTimestamp.bind(this)}> 
+                <div className="card-content grey-text text-darken-3">
+                    <span className="homescreen-card card-title">{diagram.name}</span>
+                </div>
+            </div>
+        );
+    }
+}
+export default DiagramCard;
---
+++ src/components/home_screen/DiagramLinks.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import DiagramCard from './DiagramCard';
+
+class DiagramLinks extends React.Component {
+    render() {
+        const diagrams = this.props.diagrams;
+        console.log(diagrams);
+        return (
+            <div className="todo-lists section">
+                {diagrams && diagrams.map(diagram => (
+                    <Link to={'/diagrams/' + diagram.id} key={diagram.id}>
+                        <DiagramCard diagram={diagram} />
+                    </Link>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        diagrams: state.firestore.ordered.diagrams,
+        auth: state.firebase.auth,
+    };
+};
+
+export default compose(connect(mapStateToProps))(DiagramLinks);
